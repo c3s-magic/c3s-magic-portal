efbbb1ac37c69fc47c563dbab49f22aaa8e2fd28
Niels Drost <Niels Drost>
2017-12-01 02:36:32 +0100

    added perfmetrics and config templates for esmvaltool, finished implementing perfmetrics process

--- adaguc-services-compute/processes/esmvaltool-perfmetrics.py
+++ adaguc-services-compute/processes/esmvaltool-perfmetrics.py
@@ -31,10 +31,10 @@ class Process(WPSProcess):
                             title="Model comparison report",
                             storeSupported="True",
                             statusSupported="True",
-                            abstract="Creates a report comparing models using ESMValTool (takes about 30 minutes).",
+                            abstract="Creates a (sample of a) performance metrics report comparing models using ESMValTool.",
                             grassLocation=False)
 
-	model_names = ['None', 'ACCESS1-0', 'ACCESS1-3', 'bcc-csm1-1', 'EC-EARTH', 'MIROC5']
+	model_names = ['None', 'bcc-csm1-1', 'GFDL-ESM2G', 'MPI-ESM-LR', 'MPI-ESM-MR']
 
         self.model1 = self.addLiteralInput(identifier="model1",
                                                title="Model 1",
@@ -42,7 +42,7 @@ class Process(WPSProcess):
                                                abstract="Model name",
                                                minOccurs=1,
                                                maxOccurs=1,
-                                               default='ACCESS1-0',
+                                               default='bcc-csm1-1',
                                                allowedValues=model_names)
         self.model2 = self.addLiteralInput(identifier="model2",
                                                title="Model 2",
@@ -50,7 +50,7 @@ class Process(WPSProcess):
                                                abstract="Model name",
                                                minOccurs=1,
                                                maxOccurs=1,
-                                               default='ACCESS1-3',
+                                               default='GFDL-ESM2G',
                                                allowedValues=model_names)
         self.model3 = self.addLiteralInput(identifier="model3",
                                                title="Model 3",
@@ -58,6 +58,7 @@ class Process(WPSProcess):
                                                abstract="Model name",
                                                minOccurs=1,
                                                maxOccurs=1,
+                                               default='MPI-ESM-LR',
                                                allowedValues=model_names)
         self.model4 = self.addLiteralInput(identifier="model4",
                                                title="Model 4",
@@ -65,17 +66,18 @@ class Process(WPSProcess):
                                                abstract="Model name",
                                                minOccurs=1,
                                                maxOccurs=1,
+                                               default='MPI-ESM-MR',
                                                allowedValues=model_names)
 
 	self.variable = self.addLiteralInput(identifier="variable",
                                               title="Variable",
                                               type="String",
-                                              default="tas",
+                                              default="ta",
                                               minOccurs=1,
                                               maxOccurs=1)
 
-	self.period = self.addLiteralInput(identifier="period",
-                                              title="Period of the data",
+	self.mip = self.addLiteralInput(identifier="mip",
+                                              title="MIP of the data",
                                               type="String",
                                               default="Amon",
                                               minOccurs=1,
@@ -96,23 +98,23 @@ class Process(WPSProcess):
                                               maxOccurs=1)
 
 
-        self.startYear = self.addLiteralInput(identifier="startYear",
+        self.start_year = self.addLiteralInput(identifier="start_year",
                                               title="First year data used in plot",
                                               type="Integer",
-                                              default=2003,
+                                              default=2001,
                                               minOccurs=1,
                                               maxOccurs=1)
 
-        self.endYear = self.addLiteralInput(identifier="endYear",
+        self.end_year = self.addLiteralInput(identifier="end_year",
                                             title="Last year data used in plot",
                                             type="Integer",
-                                            default=2005,
+                                            default=2002,
                                             minOccurs=1,
                                             maxOccurs=1)
 
-        self.opendapURL = self.addLiteralOutput(identifier="opendapURL",
-                                                title="opendapURL",
-                                                type="String", )
+        # self.opendapURL = self.addLiteralOutput(identifier="opendapURL",
+        #                                         title="opendapURL",
+        #                                         type="String", )
 
 	self.plot = self.addComplexOutput(identifier = "plot",
 		     title = "TimeseriesPlot",
@@ -134,13 +136,13 @@ class Process(WPSProcess):
 	        models.append(value)
 
 	variable = self.variable.getValue()
-	period = self.period.getValue()
+	mip = self.mip.getValue()
 	experiment = self.experiment.getValue()
-	start_year = self.startYear.getValue()
-        end_year = self.endYear.getValue()
+	start_year = self.start_year.getValue()
+        end_year = self.end_year.getValue()
  	ensemble_member = self.ensemble_member.getValue()
 
-        logging.debug("models %s, variable %s, period %s, experiment %s, ensemble_member %s, start_year %s, end_year %s" % (models, variable, period, experiment, ensemble_member, start_year, end_year))
+        logging.debug("models %s, variable %s, mip %s, experiment %s, ensemble_member %s, start_year %s, end_year %s" % (models, variable, mip, experiment, ensemble_member, start_year, end_year))
 
 	# This does not work atm.
         # This allows the NetCDF library to find the users credentials (X509 cert)
@@ -179,7 +181,7 @@ class Process(WPSProcess):
 
         model_descriptions = []
 	for model in models:
-	    model_descriptions.append('CMIP5_ETHZ %s %s %s %s %s %s @{MODELPATH}/ETHZ_CMIP5/' % ( model, period, experiment, ensemble_member, start_year, end_year))
+	    model_descriptions.append('- {name: %s, project: CMIP5, mip: %s, exp: %s, ensemble: %s, start_year: %s, end_year: %s}' % (model, mip, experiment,ensemble_member, start_year, end_year))
 
         self.status.set("setting up namelist for esmvaltool", 10)
 
@@ -187,36 +189,56 @@ class Process(WPSProcess):
         logging.debug("variable %s" % variable)
 
         #create esmvaltool config (using template)
-        environment = Environment(loader=FileSystemLoader('/namelists'))
+        environment = Environment(loader=FileSystemLoader('/config'))
                                   #autoescape=select_autoescape(['html', 'xml']))
 
-        template = environment.get_template('namelist_demo.xml')
+        template = environment.get_template('namelist_perfmetrics_CMIP5_template.yml')
 
-        generated_namelist = template.render(models=model_descriptions, variable=variable, work_dir=output_folder_path)
+        generated_namelist = template.render(models=model_descriptions, variable=variable, start_year=start_year, end_year=end_year)
 
-        logging.debug("template output = %s" % generated_namelist)
+        logging.debug("template output = %s" % generated_namelist) 
+
 
         #write generated namelist to file
 
-        namelist_path = output_folder_path + "/" + 'namelist_demo.xml'
+        namelist_path = output_folder_path + "/" + 'namelist_demo.yml'
 
         namelist_fd = open(namelist_path, 'w')
         namelist_fd.write(generated_namelist)
         namelist_fd.close()
 
+
+        template = environment.get_template('config-user-template.yml')
+
+        generated_config = template.render(output_folder_path=output_folder_path)
+
+        logging.debug("config template output = %s" % generated_config)
+
+        config_path = output_folder_path + "/" + 'config.yml'
+
+        config_fd = open(config_path, 'w')
+        config_fd.write(generated_config)
+        config_fd.close()
+
         #run esmvaltool command
+
+        os.environ['PYTHONIOENCODING'] = 'utf-8'
 
         self.status.set("running esmvaltool", 20)
 
         os.chdir('/src/ESMValTool')
 
-        self.cmd(['python', 'main.py', namelist_path])
+
+        self.cmd(['python', 'esmvaltool/main.py', '-c', config_path, '-n', namelist_path])
 
         #grep output from output folder
 
         self.status.set("processing output", 90)
 
-        output_image = glob.glob(output_folder_path + "/tsline/*.png").pop()
+        #output_image = glob.glob(output_folder_path + "/tsline/*.png").pop()
+        output_image = glob.glob(output_folder_path + "/plot/*_namelist_demo/perfmetrics_main/*.png").pop()
+
+        self.cmd(['mogrify', '-trim', '-bordercolor', 'white', '-border', '10x10', output_image])
 
         logging.debug("output image path is %s" % output_image)
 
@@ -227,11 +249,11 @@ class Process(WPSProcess):
 
         #KNMI WPS Specific Set output
 
-        output_nc = glob.glob(output_folder_path + "/tsline/*.nc").pop()
+        output_nc = glob.glob(output_folder_path + "/work/*_namelist_demo/perfmetrics_main/*.nc").pop()
 
         rel_output_nc = os.path.relpath(output_nc, output_folder_path)
 
         url = output_folder_url + "/" + rel_output_nc
 
-        self.opendapURL.setValue(url);
+        #self.opendapURL.setValue(url);
         self.status.set("ready", 100);
