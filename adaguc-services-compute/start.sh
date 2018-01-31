b620a8722206f106154c1f6c2c3dbcc51dc64775
maartenplieger <maartenplieger@gmail.com>
2018-01-31 14:36:09 +0100

    Fixed esmvaltool and climateexplorer in conda env

--- adaguc-services-compute/start.sh
+++ adaguc-services-compute/start.sh
@@ -41,15 +41,9 @@ keytool -import -v -trustcacerts -alias adagucservicescert -file adaguc-services
 
 HOME=$ADAGUC_SERVICES_HOME
 
-echo "Starting POSTGRESQL DB" && \
-runuser -l postgres -c "pg_ctl -w -D /postgresql -l /var/log/postgresql.log start" && \
-mkdir -p /data/adaguc-autowms/ && \
-mkdir -p /data/adaguc-datasets/ && \
-cp /src/adaguc-server/data/datasets/testdata.nc /data/adaguc-autowms/ && \
-cp /src/adaguc-server/data/config/datasets/dataset_a.xml /data/adaguc-datasets/ && \
-echo "Configuring POSTGRESQL DB" && \
-runuser -l postgres -c "createuser --superuser adaguc" && \
-runuser -l postgres -c "psql postgres -c \"ALTER USER adaguc PASSWORD 'adaguc';\"" && \
-runuser -l postgres -c "psql postgres -c \"CREATE DATABASE adaguc;\"" && \
+echo "Activating conda env"
+source activate esmvalcondaenv
+
+
 echo "Starting TOMCAT Server" && \
 java -jar /src/adaguc-services.war
