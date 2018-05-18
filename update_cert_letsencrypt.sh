4d5fc4ea3abc00b43e9cf9f355005e5353b98f4e
maartenplieger <maartenplieger@gmail.com>
2018-05-18 12:02:44 +0200

    Updated adaguc-datasets with data from WP7_ISAC_rainfarm

--- update_cert_letsencrypt.sh
+++ update_cert_letsencrypt.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
-COMPUTEHOME=adaguc-services-compute/adaguc-services-compute-home
-CONTROLLERHOME=adaguc-services-controller/adaguc-services-controller-home
+SECURITY_COMPUTE=adaguc-services-compute/security
+SECURITY_CONTROLLER=adaguc-services-controller/security
+
 HOSTNAME=portal.c3s-magic.eu
 
 echo ... convert letsencrypt certificate to PKCS12 file
@@ -9,14 +10,14 @@ echo ... convert letsencrypt certificate to PKCS12 file
 sudo openssl pkcs12 -export -in /etc/letsencrypt/live/$HOSTNAME/fullchain.pem -inkey /etc/letsencrypt/live/$HOSTNAME/privkey.pem -out fullchain_and_key.p12 -name tomcat -passout pass:password
 
 echo ... remove old keystore
-rm ${CONTROLLERHOME}/security/keystore.jks
+rm ${SECURITY_CONTROLLER}/keystore.jks
 
 echo ... create new keystore containing certificate
-keytool -importkeystore -deststorepass password -destkeystore ${CONTROLLERHOME}/security/keystore.jks -srckeystore fullchain_and_key.p12 -srcstoretype PKCS12 -srcstorepass password -alias tomcat
+keytool -importkeystore -deststorepass password -destkeystore ${SECURITY_CONTROLLER}/keystore.jks -srckeystore fullchain_and_key.p12 -srcstoretype PKCS12 -srcstorepass password -alias tomcat
 
-keytool -delete -alias letsencryptca -keystore ${CONTROLLERHOME}/security/truststore.ts -storepass changeit -noprompt
-sudo keytool -import -v -trustcacerts -alias letsencryptca -file /etc/letsencrypt/live/$HOSTNAME/chain.pem -keystore ${CONTROLLERHOME}/security/truststore.ts -storepass changeit -noprompt
+keytool -delete -alias letsencryptca -keystore ${SECURITY_CONTROLLER}/truststore.ts -storepass changeit -noprompt
+sudo keytool -import -v -trustcacerts -alias letsencryptca -file /etc/letsencrypt/live/$HOSTNAME/chain.pem -keystore ${SECURITY_CONTROLLER}/truststore.ts -storepass changeit -noprompt
 
 echo ... also use keystore for compute container
-cp ${CONTROLLERHOME}/security/keystore.jks ${COMPUTEHOME}/security/keystore.jks 
-cp ${CONTROLLERHOME}/security/truststore.ts ${COMPUTEHOME}/security/truststore.ts 
+cp ${SECURITY_CONTROLLER}/keystore.jks ${SECURITY_COMPUTE}/keystore.jks 
+cp ${SECURITY_CONTROLLER}/truststore.ts ${SECURITY_COMPUTE}/truststore.ts 
