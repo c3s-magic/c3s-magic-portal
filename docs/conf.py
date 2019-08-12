22f1bd857b5d9be21c8a9b842be7a63b65f92993
Niels Drost <n.drost@esciencecenter.nl>
2019-08-12 14:47:17 +0200

    removed m2r dependency

--- docs/conf.py
+++ docs/conf.py
@@ -44,10 +44,10 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.intersphinx',
-    'm2r'
-]
+#extensions = [
+#    'sphinx.ext.intersphinx'
+#    'm2r'
+#]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
